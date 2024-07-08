901ce64e724ad84a1ffb91ea8f361bc9723d56d8
Darfik43 <darfik43@gmail.com>
2024-07-08 21:58:40 +0300

    refactor(model): clean user model for redis to use it

--- src/main/java/com/darfik/mishanyacoin/model/User.java
+++ src/main/java/com/darfik/mishanyacoin/model/User.java
@@ -1,16 +1,19 @@
 package com.darfik.mishanyacoin.model;
 
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
 @RedisHash("User")
+@AllArgsConstructor
 public class User implements Serializable {
 
-    @Id
+    @JsonProperty
     private String id;
 
+    @JsonProperty
     private String username;
 
 }
