d1ea2637bcec2475ce1430df693c13c03e811311
Darfik43 <darfik43@gmail.com>
2024-07-12 01:07:33 +0300

    feat(model): add getter for user

--- src/main/java/com/darfik/mishanyacoin/model/User.java
+++ src/main/java/com/darfik/mishanyacoin/model/User.java
@@ -2,11 +2,13 @@ package com.darfik.mishanyacoin.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
 @RedisHash("user")
+@Getter
 @AllArgsConstructor
 public class User implements Serializable {
 
