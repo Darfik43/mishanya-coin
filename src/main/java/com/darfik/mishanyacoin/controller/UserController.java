ff2e7219d76fa07835c2c18752192c66483c9842
Darfik43 <darfik43@gmail.com>
2024-07-12 01:08:06 +0300

    feat(controller): UserController now return dto object instead of entity

--- src/main/java/com/darfik/mishanyacoin/controller/UserController.java
+++ src/main/java/com/darfik/mishanyacoin/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.darfik.mishanyacoin.controller;
 
 import com.darfik.mishanyacoin.dto.RegistrationRequest;
-import com.darfik.mishanyacoin.model.User;
+import com.darfik.mishanyacoin.dto.UserInfoResponse;
 import com.darfik.mishanyacoin.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +14,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable String id) {
+    public UserInfoResponse getUserById(@PathVariable String id) {
         return userService.getById(id);
     }
 
