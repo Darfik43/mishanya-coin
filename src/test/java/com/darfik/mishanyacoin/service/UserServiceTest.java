fa03a9d519abb293644429b9ddf64e99094a1300
Darfik43 <darfik43@gmail.com>
2024-07-11 23:47:59 +0300

    feat(test): add test class

---
+++ src/test/java/com/darfik/mishanyacoin/service/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.darfik.mishanyacoin.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    private UserService userService;
+
+}
