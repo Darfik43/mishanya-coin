e60d738a1981cb6d7a9bf660f3fd762b2e668163
Darfik43 <darfik43@gmail.com>
2024-07-23 20:43:05 +0300

    feat(model): add getter and setter annotation for user click entity

--- src/main/java/com/darfik/mishanyacoin/model/Click.java
+++ src/main/java/com/darfik/mishanyacoin/model/Click.java
@@ -1,10 +1,14 @@
 package com.darfik.mishanyacoin.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("user_clicks")
 @AllArgsConstructor
+@Getter
+@Setter
 public class Click {
 
     private String id;
