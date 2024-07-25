2bc71bf3d9bcdad99da0a2872f55fe2bae46c5a5
Darfik43 <darfik43@gmail.com>
2024-07-25 23:33:35 +0300

    feat(model): add getters and setters for coin

--- src/main/java/com/darfik/mishanyacoin/model/Coin.java
+++ src/main/java/com/darfik/mishanyacoin/model/Coin.java
@@ -3,14 +3,18 @@ package com.darfik.mishanyacoin.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("coin_price")
 @AllArgsConstructor
+@Getter
+@Setter
 public class Coin {
 
     @JsonProperty
-    private String id;  //Should be name of coin
+    private String id;
 
     @JsonProperty
     private Double price;
