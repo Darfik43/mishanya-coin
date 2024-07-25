1e12beb58789ef0d91e3f85bbafaf6e51ca39544
Darfik43 <darfik43@gmail.com>
2024-07-25 23:19:53 +0300

    feat(exception): add exception if coin exists

---
+++ src/main/java/com/darfik/mishanyacoin/exception/CoinAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.darfik.mishanyacoin.exception;
+
+public class CoinAlreadyExistsException extends RuntimeException{
+
+    public CoinAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
