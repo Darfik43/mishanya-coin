8b76ef78b174dbab87f84c615277ef714238a288
Darfik43 <darfik43@gmail.com>
2024-07-25 23:00:22 +0300

    feat(service): service receives only id

--- src/main/java/com/darfik/mishanyacoin/service/ClickServiceImpl.java
+++ src/main/java/com/darfik/mishanyacoin/service/ClickServiceImpl.java
@@ -1,10 +1,10 @@
 package com.darfik.mishanyacoin.service;
 
-import com.darfik.mishanyacoin.dto.UserInfo;
-import com.darfik.mishanyacoin.model.Click;
 import com.darfik.mishanyacoin.repository.ClickRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
@@ -13,13 +13,14 @@ public class ClickServiceImpl implements ClickService {
     private final ClickRepository clickRepository;
 
     @Override
-    public void handleClick(UserInfo userInfo) {
-        Click click = clickRepository.findById(userInfo.id()).get(); //TODO Add check if user exists
-
-        Long clickAmount = click.getClicksAmount();
-        click.setClicksAmount(++clickAmount);
-
-        clickRepository.save(click);
+    public void handleClick(String id) {
+        clickRepository.findById(id)
+                .map(click -> {
+                    click.setClicksAmount(click.getClicksAmount() + 1);
+                    clickRepository.save(click);
+                    return click;
+                })
+                .orElseThrow(() -> new NoSuchElementException("User with id " + id + " doesn't exist"));
     }
 
 }
